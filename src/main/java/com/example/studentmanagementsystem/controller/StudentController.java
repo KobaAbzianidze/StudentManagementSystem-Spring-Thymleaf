f7aa7d21da1d58c0f12e49e4523bb6d28a36fee3
koba <kobaabzianidze@gmail.com>
2022-12-20 14:19:59 +0400

    firstCommit of Spring/Thymleaf project

---
+++ src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
@@ -0,0 +1,51 @@
+package com.example.studentmanagementsystem.controller;
+
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model){
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String creatStudentForm(Model model){
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model){
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model){
+        Student existStudent = studentService.getStudentById(id);
+        existStudent.setFirstName(student.getFirstName());
+        existStudent.setLastName(student.getLastName());
+        existStudent.setEmail(student.getEmail());
+        studentService.updateStudent(existStudent);
+        return "redirect:/students";
+    }
+}
